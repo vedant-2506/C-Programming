ded48b217490d2142fdc41b4e8f44f7c3ebeeb90
vedant-2506 <swamidivate2506@gmail.com>
2025-07-28 22:52:25 +0530

    delete all

--- Practice/float.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h> 
-int main()
-{
-
-    float pi=3.14;
-    printf("Value of pi is %f \n", pi);
-    return 0;
-
-}
--- Practice/integer.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-int i =10;
-printf("iteger is %d \n",i);
-return 0;
-}
