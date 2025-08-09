48f4da5803329b26e196fe39862764fdfe3a52e0
vedant-2506 <swamidivate2506@gmail.com>
2025-08-09 22:09:56 +0530

    accept 2 number and check nuber is odd or even

--- Practice/condition/Ternary/ex2.c
+++ Practice/condition/Ternary/ex2.c
@@ -1,3 +1,16 @@
 //Use a ternary operator to find if a number is even or odd.
 
+#include<stdio.h>
+int main ()
+{
+    int a=0;
 
+    printf("Check numbr id odd or even \n");
+    printf("Enter number :");
+    scanf("%d",&a);
+
+    a%2==0?printf("%d is even number \n",a):printf("%d is odd number \n",a);
+
+    return 0;
+    
+}
