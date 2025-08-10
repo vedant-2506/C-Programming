8dc90eaadf921fe5cff56a362688919308e66b7b
vedant-2506 <swamidivate2506@gmail.com>
2025-08-10 22:35:22 +0530

    enter  number  of month and print name  of month

---
+++ Practice/Switch/ex3.c
@@ -0,0 +1,67 @@
+//Input a number (1–12) and display the name of the month via switch.
+
+#include<stdio.h>
+int main()
+{
+    int n=0;
+
+    printf("Enter numer  as sequence of month and check which month on this number   :");
+    scanf("%d",&n);
+
+    switch(n) 
+    {
+        case 1:
+        printf("January. \n");
+        break;
+
+        case 2:
+        printf("February. \n");
+        break;
+
+       case 3:
+        printf("March. \n");
+        break;
+
+        case 4:
+        printf("April. \n");
+        break;
+
+        case 5:
+        printf("May. \n");
+        break;
+
+        case 6:
+        printf("June. \n");
+        break;
+
+        case 7:
+        printf("July. \n");
+        break;
+
+        case 8:
+        printf("August. \n");
+        break;
+
+        case 9:
+        printf("September. \n");
+        break;
+
+       case 10:
+        printf("October. \n");
+        break;
+
+        case 11:
+        printf("November. \n");
+        break;
+
+        case 12:
+        printf("December. \n");
+        break;
+
+        defult:
+        printf("Invaild Number");
+
+    }
+
+    return 0;
+}
