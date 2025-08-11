6b350ad0e5fc18cd26b52a8f9a5504c404a5e206
vedant-2506 <swamidivate2506@gmail.com>
2025-08-11 21:57:26 +0530

    print value 1 to 10

---
+++ Practice/Loop/for/ex1.c
@@ -0,0 +1,15 @@
+//Print all numbers from 1 to n (user input 
+
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    for(i=0;i<=10;i++)
+    {
+        printf("%d \n",i);
+  
+    }
+
+     return 0;
+
+}
