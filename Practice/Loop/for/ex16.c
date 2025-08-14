df06c65b543a4ece2a5d359ea648529acbb38834
vedant-2506 <swamidivate2506@gmail.com>
2025-08-14 23:08:33 +0530

    check if a number is a palindrome

---
+++ Practice/Loop/for/ex16.c
@@ -0,0 +1,39 @@
+//check if a number is a palindrome
+
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    int n=0 ;
+    int ans=0;
+    int r=0;
+
+
+   
+    printf("Enter a number and reverse  :");
+    scanf("%d",&n);
+
+    int  n1=n;
+
+
+    for(;n>0;i++)
+    {
+        
+       r=n%10;
+       ans=(ans*10)+r;
+       n=n/10;
+    }
+
+    if(ans=n1)
+    {
+        printf("%d is a palindrome \n",n1);
+    }
+    else
+    {
+        printf("%d is not a palindrome \n",n1);
+    }
+
+    return 0;
+
+}
