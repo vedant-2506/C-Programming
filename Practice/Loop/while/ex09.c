bb1dc87f5fea99981fdec8e64b2bde68077f1dd1
vedant-2506 <swamidivate2506@gmail.com>
2025-08-17 20:25:00 +0530

    Count and display the number of digits in a number

---
+++ Practice/Loop/while/ex09.c
@@ -0,0 +1,21 @@
+//Count and display the number of digits in a number
+
+#include<stdio.h>
+int main()
+{
+    int n=0,r=0 ,count=0;
+
+    printf("Enter number and count the number :");
+    scanf("%d",&n);
+
+    while(n>0)
+    {
+        r=n%10;
+        count ++ ;
+        n=n/10;
+    }
+
+    printf("Number of digit is %d \n",count);
+
+    return 0;
+}
