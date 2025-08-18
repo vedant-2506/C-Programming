3b4fb723d5260949a1c4d5f91a491f6164bf6327
vedant-2506 <swamidivate2506@gmail.com>
2025-08-18 23:19:38 +0530

    print a square/triangle/star pattern based on user input (height)~

---
+++ Practice/Loop/while/ex15_tri.c
@@ -0,0 +1,25 @@
+//print a square/triangle/star pattern based on user input (height)
+
+#include<stdio.h>
+int main()
+{
+    int j=0,i=0, n=0;
+    
+
+    printf("Enter number to row of square star pattern   :");
+    scanf("%d",&n);
+
+    while(i<n)
+    {
+        while(j<=n)
+        {
+            printf("* ");
+            j++;
+        }
+     
+        printf("\n ");
+        i++;
+    }
+
+    return 0;
+}
