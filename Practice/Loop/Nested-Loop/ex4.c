e0af8d80e1fdb29f7b1d795bbbaec86026ed0b88
vedant-2506 <swamidivate2506@gmail.com>
2025-08-26 16:05:16 +0530

    Amicable numbers

---
+++ Practice/Loop/Nested-Loop/ex4.c
@@ -0,0 +1,27 @@
+// Write a program to determine whether two given numbers are amicable numbers
+// a.​ Two numbers are said to be amicable if the sum of proper divisors (excluding the
+// number itself) of each number equals the other number - (220/284)
+
+#include <stdio.h>
+
+int main() {
+    int a, b, i, sumA = 0, sumB = 0;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for (i = 1; i < a; i++)
+        if (a % i == 0) sumA += i;
+
+    for (i = 1; i < b; i++)
+        if (b % i == 0) sumB += i;
+
+    if (sumA == b && sumB == a)
+        printf("%d and %d are Amicable numbers\n", a, b);
+    else
+        printf("%d and %d are NOT Amicable numbers\n", a, b);
+
+    return 0;
+}
+
+
