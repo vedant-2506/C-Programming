a87864a9b4d53016e22525ab1a376959550c46c3
vedant-2506 <swamidivate2506@mail.com>
2025-08-28 21:59:02 +0530

    Question 1

--- Test-2/Que1.c
+++ Test-2/Que1.c
@@ -1,1 +1,30 @@
 //Question 1
+#include<stdio.h>
+int main()
+{
+    int amt;
+
+    printf("Enter your amount:");
+    scanf("%d",&amt);
+
+    if(amt >200)
+    {
+        printf("You get 30%, discount on %d. \n",amt);
+    }
+    else if(amt >101 && amt <=200)
+    {
+        printf("You get 20%, discount on %d. \n",amt);
+    }
+    else if(amt <= 100 && amt >50)
+    {
+        printf("You get 10%, discount on %d. \n",amt);
+    }
+    else
+    {
+        printf("You don't get  discount on %d. \n",amt);
+    }
+
+    return 0;
+
+
+}
