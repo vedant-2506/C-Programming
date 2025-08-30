b047f90efd6b233a68217ddc822a614bfe957c8b
vedant-2506 <swamidivate2506@gmail.com>
2025-08-30 22:58:19 +0530

    Print various formatted patterns (pyramid, diamond, Pascal’s triangle) using nested loops.

--- Practice/Loop/Nested-Loop/ex7.c
+++ Practice/Loop/Nested-Loop/ex7.c
@@ -2,15 +2,17 @@
 
 #include <stdio.h>
 
-int main() {
-    int n, i, j, space;
+int main() 
+{
+    int n, i, k, space;
     printf("Enter rows: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) 
+    {
         for (space = 1; space <= n - i; space++)
             printf(" ");
-        for (j = 1; j <= 2 * i - 1; j++)
+        for (k = 1; k <= 2 * i - 1; k++)
             printf("*");
         printf("\n");
     }
