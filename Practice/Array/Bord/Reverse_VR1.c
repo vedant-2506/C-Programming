0506beb3d160ce4d1b340a64132042c4bff6e2c8
vedant-2506 <swamidivate2506@gmail.com>
2025-09-18 21:41:42 +0530

    Reverse the whole array .Using extra memory space.

---
+++ Practice/Array/Bord/Reverse_VR1.c
@@ -0,0 +1,61 @@
+//Reverse the whole array .Using extra memory space.
+
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int *A;
+    int *B;
+    int i=0 ,n=0,j=0;
+
+    //Allocate memory using dynamic Array type 
+    printf("Enter array size :");
+    scanf("%d",&n);
+
+    A =(int *)malloc(sizeof(int) *n);
+    B =(int *)malloc(sizeof(int) *n);
+    if(A == NULL || B == NULL)
+    {
+        printf("Memory not found ");
+        exit(-1);
+    }
+
+    //Enter values in Array from user 
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    
+    //print Array
+    printf("Array is \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d \t",A[i]);
+    }
+    printf("\n");
+
+    //Revers the array
+
+ 
+    i=n-1;
+    j=0;
+
+    while(i >= 0)
+    {
+        B[j]=A[i];
+        i--;
+        j++;
+    }
+     
+    //print Array B 
+    printf("Array After reverse \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d \t",B[i]);
+    }
+    printf("\n");
+
+    free(A);
+
+    return 0;
+}
