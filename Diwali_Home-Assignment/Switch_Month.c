b34b1a8184046980edbc0be7b16c7f9697b3bab4
Vedant Divate <swamidivate2506@gmail.com>
2025-10-28 22:14:45 +0530

    Add files via upload

---
+++ Diwali_Home-Assignment/Switch_Month.c
@@ -0,0 +1,28 @@
+// Input a number (1–12) and display the name of the month via switch. 
+
+#include <stdio.h>
+
+int main() {
+    int month;
+
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+
+    switch(month) {
+        case 1: printf("January\n"); break;
+        case 2: printf("February\n"); break;
+        case 3: printf("March\n"); break;
+        case 4: printf("April\n"); break;
+        case 5: printf("May\n"); break;
+        case 6: printf("June\n"); break;
+        case 7: printf("July\n"); break;
+        case 8: printf("August\n"); break;
+        case 9: printf("September\n"); break;
+        case 10: printf("October\n"); break;
+        case 11: printf("November\n"); break;
+        case 12: printf("December\n"); break;
+        default: printf("Invalid Month Number! Enter value between 1 to 12.\n");
+    }
+
+    return 0;
+}
