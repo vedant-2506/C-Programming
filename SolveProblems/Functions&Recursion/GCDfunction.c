af21681095826adced05fc0121889d95526a5492
vedant-2506 <swamidivate2506@gmail.com>
2026-02-14 21:39:26 +0530

    Find the GCD (HCF) of two numbers using recursion.

---
+++ SolveProblems/Functions&Recursion/GCDfunction.c
@@ -0,0 +1,21 @@
+ //Find the GCD (HCF) of two numbers using recursion.
+
+ #include <stdio.h>
+
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("GCD of %d and %d = %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
