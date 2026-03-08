3e170800cb32fdf265a33e32fdd31c73e8d9c5c3
vedant-2506 <swamidivate2506@gmail.com>
2026-03-08 13:53:12 +0530

    Write a recursive function to reverse a string in place.

---
+++ SolveProblems/Functions&Recursion/RecursivReverse.c
@@ -0,0 +1,37 @@
+//Write a recursive function to reverse a string in place.
+
+#include <stdio.h>
+#include <string.h>
+
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permute(char str[], int l, int r) {
+    if (l == r) {
+        printf("%s\n", str);
+        return;
+    }
+
+    for (int i = l; i <= r; i++) {
+        swap(&str[l], &str[i]);       // Choose
+        permute(str, l + 1, r);       // Explore
+        swap(&str[l], &str[i]);       // Backtrack
+    }
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int n = strlen(str);
+
+    printf("Permutations are:\n");
+    permute(str, 0, n - 1);
+
+    return 0;
+}
